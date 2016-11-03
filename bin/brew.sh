905fcba7736230ecf061a6febf3977ac00425097
Alberto Quesada <qa.alberto@gmail.com>
2016-11-02 22:53:27 -0300

    d alias for Developer folder

---
+++ bin/brew.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# brew.sh
+# Install command-line tools using Homebrew.
+#
+# Original code and idea from:
+#   https://github.com/mathiasbynens/dotfiles/blob/master/brew.sh
---
+++ bin/macOS.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+# macOS.sh
+# Script to set up a new mac with my preferred configurations.
+#
+# Original code and idea from:
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
