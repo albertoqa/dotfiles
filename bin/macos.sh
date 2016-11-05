44bd78186beadc2ef03cf27b23feccce1408d17b
Alberto Quesada <qa.alberto@gmail.com>
2016-11-05 19:27:33 +0100

    text edit preferences

--- bin/macos.sh
+++ bin/macos.sh
@@ -582,7 +582,14 @@ echo "Configuring text edit options"
 # Plain text by default
 defaults write com.apple.TextEdit RichText -int 0
 
-# TODO text edit comillas feas!
+# Turn off 'smart' editing
+defaults write com.apple.TextEdit SmartDashes -int 0
+defaults write com.apple.TextEdit SmartQuotes -int 0
+defaults write com.apple.TextEdit TextReplacement -int 0
+
+# Open and save files as UTF-8 in TextEdit
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
 
 ###############################################################################
 # Activity Monitor                                                            #
