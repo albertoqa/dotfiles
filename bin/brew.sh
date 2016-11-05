5e90d7affa6f7134e79b5d9754d13a921ab1722e
Alberto Quesada <qa.alberto@gmail.com>
2016-11-05 12:30:25 +0100

    no need of dev tools

--- bin/brew.sh
+++ bin/brew.sh
@@ -2,15 +2,16 @@
 
 # brew.sh
 # Install command-line tools using Homebrew.
-
+#
 # IMPORTANT:
-# Before run this script you need to install Xcode, agree to the license and
-# install the command line developer tools. You can do it with:
-# sudo xcodebuild -license
-# sudo xcode-select --install
+# Before run this script you need to install Xcode.
+
+# agree to xcode license
+sudo xcodebuild -license
 
 # Install brew (if not already installed)
 if ! command -v brew >/dev/null 2>&1; then
+  echo "Installing brew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi;
 
@@ -25,14 +26,118 @@ fi
 brew update
 brew upgrade
 
+###############################################################################
+# Tools and languages                                                         #
+###############################################################################
+
 # Install python (macOS version is outdated) and python3
 # Both are compatibles and this way you can test the code in both
 brew install python python3
 
+pip install virtualenv            # install virtualenv
+
+brew install scala                # install scala language
+
+brew install ack                  # search for things within a repo
+brew install the_silver_searcher  # alternative to ack maybe faster
+brew install wget                 # similar to curl
+brew install speedtest_cli        # test internet bandwidth using speedtest.net
+brew install ssh-copy-id          # install your public key in a remote machine's authorized_keys
+brew install rename               # rename files easy
+brew install htop                 # better top replacement
+brew install tree                 # display full directory hierarchy
+brew install pstree               # display running processes as a tree
+brew install pv                   # show progress bar in pipelines
+brew install iftop                # show network usage (similar to top)
+brew install unrar                # decompress rar files
+brew install dockutil             # manage dock items from the terminal
+
+brew install maven
+brew install node
+brew install mongodb
+brew install mysql
+
+brew install sonar
+brew install sonar-runner
+
+###############################################################################
+# Cask                                                                        #
+###############################################################################
+
 # Install cask so we can install apps like ninjas
 brew tap caskroom/cask
 
+# Allow completion on brew cask
+brew install homebrew/completions/brew-cask-completion
 
+###############################################################################
+# Quicklook                                                                   #
+###############################################################################
+
+brew cask install qlmarkdown        # preview Markdown files
+brew cask install qlstephen         # preview plain text files without a extension
+brew cask install qlcolorcode       # preview source code with syntax highlight
+brew cask install quicklook-json    # preview JSON files
+brew cask install quicklook-csv     # preview CSV files
+brew cask install qlprettypatch     # preview diffs
+brew cask install betterzipql       # preview archives
+
+# Enable text selection in QuickLook views. TODO check
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+
+###############################################################################
+# Apps                                                                        #
+###############################################################################
+
+# Install all our favourite apps without even a single click 😮
+
+# Social
+brew cask install adium
+brew cask install skype
+brew cask install slack
+brew cask install whatsapp
+
+# Office
+brew cask install macdown
+# TODO missing pages, numbers, keynote
+# TODO missing Pocket
+
+# Utilities
+brew cask install flux
+brew cask install the-unarchiver
+# TODO missing Magnet
+# TODO missing CopyClip
+# TODO missing Memory Clean
+# TODO missing Commander One Pro
+
+# General
+brew cask install google-drive
+brew cask install google-chrome
+brew cask install firefox
+brew cask install spotify
+brew cask install vlc
+brew cask install transmission
+
+# Development
+brew cask install java
+brew cask install atom
+brew cask install postman
+brew cask install github-desktop
+brew cask install intellij-idea
+brew cask install pycharm
+brew cask install android-studio
+brew cask install textwrangler
+brew cask install netbeans-java-ee
+brew cask install mysqlworkbench
+# TODO missing scenebuilder
+
+# Graphics
+brew install icons8
+brew install image2icon
+brew install imagealpha
+brew install imageoptim
+# TODO missing Color Picker
+# TODO missing Promotee
 
 # Cleanup
 brew cleanup
